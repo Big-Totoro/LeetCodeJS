672446c6a659d10300bfecbaffac3311cfc3085e
Stanislav Skuratov <s.a.skuratov@gmail.com>
2021-08-31 13:16:12 +0600

    Added #389

--- problems/_389.js
+++ problems/_389.js
@@ -4,26 +4,21 @@
  * @return {string}
  */
 const findTheDifference = function (s, t) {
-    const map = new Map();
+    const arr = Array(300).fill(0);
 
-    for (let i = 0; i < s.length; i++) {
-        let count = 1;
-        if (map.has(s.charAt(i))) {
-            count = map.get(s.charAt(i)) + 1;
-        }
-        map.set(s.charAt(i), count);
+    for (let i = 0; i < s.length; ++i) {
+        const sPosition = s.codePointAt(i);
+        arr[sPosition] = arr[sPosition] + 1;
+
+        const tPosition = t.codePointAt(i);
+        arr[tPosition] = arr[tPosition] - 1;
     }
+    const tPosition = t.codePointAt(t.length - 1);
+    arr[tPosition] = arr[tPosition] - 1;
 
-    for (let i = 0; i < t.length; i++) {
-        if (map.has(t.charAt(i))) {
-            let count = map.get(t.charAt(i)) - 1;
-            if (count <= 0) {
-                map.delete(t.charAt(i));
-            } else {
-                map.set(t.charAt(i), count);
-            }
-        } else {
-            return t.charAt(i);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            return String.fromCharCode(i);
         }
     }
 };
